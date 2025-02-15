964a4d30d817528ec60f545d6780cd22d9443ce0
Grisha-3108 <Grisha-3108@yandex.ru>
2025-02-15 13:58:47 +0300

    Всё

--- src/main/java/CheckTester.java
+++ src/main/java/CheckTester.java
@@ -39,7 +39,7 @@ public class CheckTester implements AbstractTester{
 
             // Выбор коллекции
             MongoCollection<Document> collection = database.getCollection(collectionName); // Имя коллекции
-            System.out.println("Количество документов в коллекции " + collection.countDocuments());
+            //System.out.println("Количество документов в коллекции " + collection.countDocuments());
 
             // Создание документа из JSON
             Document doc = Document.parse(document);
